d9a282140f181c6afb01f615cc32b3b03a7fa6b5
Thiago <thiagovernetti@hotmail.com>
2025-11-05 11:04:09 -0300

    Alterações para criar o ambiente de produção no render

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,23 +2,28 @@ import { DataSource } from 'typeorm';
 import { Categoria } from './Model/categoria';
 import { Produto } from './Model/produto';
 import { Recompensa } from './Model/recompensa';
-import {Usuario} from './Model/usuario';
-import {Apoio} from './Model/apoio';
+import { Usuario } from './Model/usuario';
+import { Apoio } from './Model/apoio';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'floripa96',
-  database: 'crowdfunding',
-  synchronize: true,
-  logging:true,
-  entities: [Categoria,Produto,Recompensa,Usuario,Apoio],
+  url: process.env.DATABASE_URL, // Para produção no Render
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '3000'),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'floripa96',
+  database: process.env.DB_NAME || 'crowdfunding',
+  synchronize: process.env.NODE_ENV !== 'production', // true apenas em dev
+  logging: process.env.NODE_ENV !== 'production', // true apenas em dev
+  entities: [Categoria, Produto, Recompensa, Usuario, Apoio],
   migrations: [],
   subscribers: [],
-  
+  ...(process.env.NODE_ENV === 'production' && {
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  })
 });
-const queryBuilder = AppDataSource.createQueryBuilder
-console.log('Conectando ao banco:', AppDataSource.options.database);
-
---
+++ src/migrations/1762351400159-InitialSetup.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialSetup1762351400159 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
