59b76151f700f9199830284d60a1d880e1f611d6
Thiago <thiagovernetti@hotmail.com>
2025-11-10 12:46:29 -0300

    alteração no arquivo de multer

--- src/Config/multer.ts
+++ src/Config/multer.ts
@@ -1,18 +1,19 @@
 import multer from 'multer';
 import path from 'path';
 import { Request } from 'express';
+import type { FileFilterCallback } from 'multer';
 
 const storage = multer.diskStorage({
-  destination: (req: Request, file, cb) => {
+  destination: (req: Request, file: Express.Multer.File, cb) => {
     cb(null, 'uploads/');
   },
-  filename: (req: Request, file, cb) => {
+  filename: (req: Request, file: Express.Multer.File, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
   }
 });
 
-const fileFilter = (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
+const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
   if (file.mimetype.startsWith('image/')) {
     cb(null, true);
   } else {
@@ -24,7 +25,7 @@ const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
   limits: {
-    fileSize: 5 * 1024 * 1024 // Limite de 5MB
+    fileSize: 5 * 1024 * 1024
   }
 });
 
