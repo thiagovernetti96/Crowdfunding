575750fd322cff089aafa2b7257d40dfb5473080
Thiago <thiagovernetti@hotmail.com>
2025-11-10 12:48:45 -0300

    simplificação do multer

--- src/Config/multer.ts
+++ src/Config/multer.ts
@@ -1,19 +1,17 @@
 import multer from 'multer';
 import path from 'path';
-import { Request } from 'express';
-import type { FileFilterCallback } from 'multer';
 
 const storage = multer.diskStorage({
-  destination: (req: Request, file: Express.Multer.File, cb) => {
+  destination: (req, file, cb) => {
     cb(null, 'uploads/');
   },
-  filename: (req: Request, file: Express.Multer.File, cb) => {
+  filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
   }
 });
 
-const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image/')) {
     cb(null, true);
   } else {
